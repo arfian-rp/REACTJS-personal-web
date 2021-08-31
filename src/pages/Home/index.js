975a2dfce69f5e5ead67cb41cade73dfa7ee5f52
arfian-rp <arfianrafi77@gmail.com>
2021-08-31 13:36:42 +0800

    ok

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,7 +4,7 @@ function Home(props) {
   return (
     <div className="container text-center">
       <h4>Hello, i'am</h4>
-      <img src="/img/my_photo.jpg" className="mt-2 mb-2 rounded-circle img-thumbnail" width="150" alt="arfian" />
+      <img src="/img/profil.jpg" className="mt-2 mb-2 rounded-circle img-thumbnail" width="150" alt="arfian" />
       <h1 className="display-5">Arfian Pradana</h1>
       <p className="lead">student | professional google search | weeb</p>
     </div>
