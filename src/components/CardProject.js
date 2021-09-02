aface11e19d88ab764d70cb8b88eaff801cc89a9
arfian pradana <80876671+arfian-rp@users.noreply.github.com>
2021-09-02 13:52:18 +0800

    ok

--- src/components/CardProject.js
+++ src/components/CardProject.js
@@ -3,7 +3,7 @@ import React from "react";
 function CardProject(props) {
   return (
     <div className="col-md-3 mb-4">
-      <div className="card" style={{ width: "18rem" }}>
+      <div className="card">
         <img src={props.img} className="card-img-top" alt={props.alt} />
         <div className="card-body">
           <h5 className="card-title">{props.title}</h5>
