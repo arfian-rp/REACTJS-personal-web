28e8ad3a5bfd4f75a4ca03a4b7955c531ce29507
arfian pradana <80876671+arfian-rp@users.noreply.github.com>
2021-09-10 20:03:37 +0800

    ok

--- src/pages/Profile/About.js
+++ src/pages/Profile/About.js
@@ -10,7 +10,7 @@ function About(props) {
           My name is Arfian Pradana from Surabaya.
           <br />I was born in Douglas on January 14, 2007.
         </p>
-        <LinkToSosmed icon="bi bi-instagram me-5" url="https://instagram.com/arfian_rp__" />
+        <LinkToSosmed icon="bi bi-instagram me-5" url="https://instagram.com/arfian_rp_" />
         <LinkToSosmed icon="bi bi-twitter me-5" url="https://twitter.com/ArfianRp" />
         <LinkToSosmed icon="bi bi-github me-5" url="https://github.com/arfian-rp" />
       </div>
