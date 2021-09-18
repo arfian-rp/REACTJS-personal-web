92db1719ed46068016aaafb19556b9d30edd6fce
arfian pradana <80876671+arfian-rp@users.noreply.github.com>
2021-09-18 16:48:08 +0800

    ok

--- src/pages/Profile/Myskills.js
+++ src/pages/Profile/Myskills.js
@@ -10,17 +10,6 @@ function Myskills(props) {
       <SkillBar name="HTML" value="92" />
       <SkillBar name="Javascript & Nodejs" value="90" />
       <SkillBar name="CSS" value="85" />
-      <SkillBar name="PHP" value="85" />
-      <SkillBar name="Java" value="75" />
-      <SkillBar name="ExpressJS" value="80" />
-      <SkillBar name="ReactJS" value="60" />
-      <SkillBar name="RecoilJS" value="90" />
-      <SkillBar name="Mysql DB" value="60" />
-      <SkillBar name="Mongoose DB" value="60" />
-      <SkillBar name="JQuery" value="80" />
-      <SkillBar name="Bootstrap" value="70" />
-      <SkillBar name="VueJS" value="60" />
-      <SkillBar name="Codeigniter" value="40" />
     </div>
   );
 }
